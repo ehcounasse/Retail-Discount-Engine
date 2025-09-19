f8884510879c8622157885d089148aaac7e48a83
Ethan Counasse <ehcounasse@usf.edu>
2025-09-19 08:29:55 -0400

    Created a Array with 5 Products

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,9 @@
 // Create a Array of 5 Objects
+
+let product = [
+    {name:'Keyboard', category:'electronics', price: 21.99, inventory: 142},
+    {name:'Hoodie', category: 'apparel', price: 14.99, inventroy: 201},
+    {name:'Apple', category:'groceries', price: 1.99, inventory: 607},
+    {name:'Lamp', category:'household', price: 10.99, inventory: 93},
+    {name:'Hammer', category: 'hardware', price: 12.99, inventory: 481}
+];
